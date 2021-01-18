24f1cfcd954d856d910b30781db07e9719357d87
henrisama <pedropiresflores@icloud.com>
2021-01-18 16:38:11 -0400

    firt commit

---
+++ client/src/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+    render(){
+        return(
+            <div className="bg-dark h-100">
+                <h1>Teste</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
